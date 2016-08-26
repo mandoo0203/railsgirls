94b1b95c64e225a8685a73ef97ce7d5a7362b234
Cho <mandoo020374@gmail.com>
2016-08-26 15:31:01 +0900

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,12 @@
+Rails.application.routes.draw do
+  resources :comments
+  get 'pages/info'
+
+  resources :ideas
+  root to: redirect('/ideas')
+
+  get "pages/info"
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
